6fa8e1355c93e28bb72f5b7d5ce27d8619acb3b5
Michael Chadwick <michael.chadwick@gmail.com>
2024-07-11 11:45:30 -0700

    more vue cypress configuration

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -21,7 +21,26 @@ import './commands'
 
 import { mount } from 'cypress/vue'
 
-Cypress.Commands.add('mount', mount)
+Cypress.Commands.add('mount', (component, options = {}) => {
+  // Setup options object
+  options.global = options.global || {}
+  options.global.stubs = options.global.stubs || {}
+  options.global.stubs['transition'] = false
+  options.global.components = options.global.components || {}
+  options.global.plugins = options.global.plugins || []
+
+  /* Add any global plugins */
+  // options.global.plugins.push({
+  //   install(app) {
+  //     app.use(MyPlugin);
+  //   },
+  // });
+
+  /* Add any global components */
+  // options.global.components['Button'] = Button;
+
+  return mount(component, options)
+})
 
 // Example use:
 // cy.mount(MyComponent)
